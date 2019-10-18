fb142528d007f26db227620b877200e23b9fd683
Andrey Gruber <andrey.gruber@gmail.com>
2019-10-19 01:25:53 +0300

    Added GlobalSuppressions, updated Oracle provider

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:ag.DbData.Oracle.OracleDbDataObject.innerFillDataSet(System.String,System.Collections.Generic.IEnumerable{System.String},System.Int32,System.Boolean)~System.Data.DataSet")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:ag.DbData.Oracle.OracleDbDataObject.innerFillDataTable(System.String,System.Int32,System.Boolean)~System.Data.DataTable")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:ag.DbData.Oracle.OracleDbDataObject.innerExecuteAsync(System.String,System.Threading.CancellationToken,System.Int32)~System.Threading.Tasks.Task{System.Int32}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:ag.DbData.Oracle.OracleDbDataObject.innerGetScalarAsync(System.String,System.Threading.CancellationToken,System.Int32)~System.Threading.Tasks.Task{System.Object}")]
